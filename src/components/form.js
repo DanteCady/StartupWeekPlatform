9217bcea7c5177b71e2f435a3b9318c7dfa78170
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-06 21:38:56 -0400

    Adjust form component styling - removing grid

--- src/components/form.js
+++ src/components/form.js
@@ -5,13 +5,20 @@ import Grid  from '@mui/material/Grid2';
 // FormComponent that take props from the form config util
 const FormComponent = ({ formConfig }) => {
     return (
-        <Box>
-            <Grid container spacing={3}>
+        <Box
+        sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            height: '100%',
+            padding: '2rem',
+        }}
+        >
                 {/* Loop through the fields array and render the form fields */}
                 {formConfig.fields.map((field) => (
-                    <Grid item xs={12} sm={6} md={4} key={field.name}>
                         <TextField
-                            fullWidth
                             name={field.name} // name attribute for the form field
                             label={field.label} // label for the form field
                             required={field.required} // required attribute for the form field
@@ -19,14 +26,10 @@ const FormComponent = ({ formConfig }) => {
                             rows={field.rows} // rows attribute for the form field 
                             helperText={field.helperText} // helperText attribute for the form field
                         />
-                    </Grid>
                 ))}
-                <Grid item xs={12}>
                     <Button type="submit" variant="outlined" color="success">
                         <Typography variant="button">Submit</Typography>
                     </Button>
-                </Grid>
-            </Grid>
         </Box>
     );
 };
