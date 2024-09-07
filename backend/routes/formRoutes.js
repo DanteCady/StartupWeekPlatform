1cfbd5dea7d25739e15551b8ed8f0c2b2ff4872d
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-07 11:08:41 -0400

    Updated form position

--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -10,9 +10,9 @@ module.exports = (databasePool) => {
 
     router.post('/submit', async (req, res) => {
         try {
-            const { firstName, lastName, email, phoneNumber, event, affiliation } = req.body; // Destructure the request body
-            const query = 'INSERT INTO event_submissions (firstName, lastName, email, phoneNumber, event, affiliation) VALUES (?, ?, ?, ? ,? ,?)'; // SQL query to insert form data
-            const [result] = await databasePool.query(query, [firstName, lastName, email, phoneNumber, event, affiliation]); // Execute the query
+            const { firstName, lastName, email, phoneNumber, affiliation } = req.body; // Destructure the request body
+            const query = 'INSERT INTO event_submissions (firstName, lastName, email, phoneNumber, affiliation) VALUES (?, ?, ?, ? ,? )'; // SQL query to insert form data
+            const [result] = await databasePool.query(query, [firstName, lastName, email, phoneNumber, affiliation]); // Execute the query
             res.status(200).send(result); // Send the result
         } catch (error) {
             res.status(500).send(error); // Send the error
