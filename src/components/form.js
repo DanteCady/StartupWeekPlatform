6a4db79661c812c161e4d9147760ee7d54fa8fc6
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-06 22:48:28 -0400

    Update UI to form

--- src/components/form.js
+++ src/components/form.js
@@ -1,49 +1,68 @@
 import React from 'react';
 import { TextField, Button, Box, Typography } from '@mui/material';
-import Grid  from '@mui/material/Grid2';
+import Grid from '@mui/material/Grid2';
 
 // FormComponent that take props from the form config util
 const FormComponent = ({ formConfig }) => {
-    return (
-        <Box
-        sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-            width: '100%',
-            mt: '2rem',
-        }}
-        >
-                {/* Loop through the fields array and render the form fields */}
-                {formConfig.fields.map((field) => (
-                        <TextField
-                            name={field.name} // name attribute for the form field
-                            label={field.label} // label for the form field
-                            required={field.required} // required attribute for the form field
-                            multiline={field.multiline} // multiline attribute for the form field
-                            rows={field.rows} // rows attribute for the form field 
-                            helperText={field.helperText} // helperText attribute for the form field
-                            sx={{
-                                width: '100%',
-                                marginTop: '1rem',
-                            }}
-                        />
-                ))}
-                    <Button 
-                    type="submit" 
-                    variant="outlined" 
-                    color="#252b4e"
-                    sx={{
-                        marginTop: '1rem',
-                        width: '100%',
-                        borderRadius: '8px',
-                    }}>
-                        <Typography variant="button">Register</Typography>
-                    </Button>
-        </Box>
-    );
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+				height: '100%',
+				width: '100%',
+				mt: '2rem',
+			}}
+		>
+			{/* Loop through the fields array and render the form fields */}
+			{formConfig.fields.map((field) => (
+				<TextField
+					name={field.name} // name attribute for the form field
+					label={field.label} // label for the form field
+					required={field.required} // required attribute for the form field
+					multiline={field.multiline} // multiline attribute for the form field
+					rows={field.rows} // rows attribute for the form field
+					helperText={field.helperText} // helperText attribute for the form field
+					fullWidth // fullWidth attribute for the form field
+					variant="standard" // variant attribute for the form field
+					slotProps={{
+						input: {
+							style: { color: '#fff' }, // Change input text color to white
+						},
+						inputLabel: {
+							style: { color: '#fff' }, // Change label text color to white
+						},
+                        formHelperText: {
+                            style: { color: '#fff' }, // Change helper text color to white
+                        },
+					}}
+					sx={{
+						marginTop: '1rem',
+						'& .MuiInput-underline:before': {
+							borderBottomColor: '#fff', // White underline before focus
+						},
+						'& .MuiInput-underline:after': {
+							borderBottomColor: '#fff', // White underline after focus
+						},
+					}}
+				/>
+			))}
+			<Button
+				type="submit"
+				variant="outlined"
+				color="#252b4e"
+				sx={{
+					marginTop: '1rem',
+					width: '100%',
+					borderRadius: '8px',
+				}}
+			>
+				<Typography variant="button">Register</Typography>
+			</Button>
+		</Box>
+	);
 };
 
 export default FormComponent;
--- src/views/home/page.js
+++ src/views/home/page.js
@@ -4,50 +4,59 @@ import FormComponent from '../../components/form';
 import { eventRegistrationForm } from '../../utils/formConfiguration';
 
 const page = () => {
-  return (
-    // Main container for the page
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-start', 
-        justifyContent: 'flex-start',
-        width: '100%',
-        height: '100%',
-        mt: '2rem',
-        position: 'relative', 
-      }}
-    >
-      {/* Box component that wraps the form */}
-      <Box 
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          width: '100%',
-          maxWidth: '400px',
-          padding: '2rem',
-          border: '1px solid #252b4e',
-          borderRadius: '8px',
-          position: 'relative',
-          left: '-300px',
-          mt: '2rem',
-        }}
-      >
-        <Typography variant="h5" gutterBottom>
-          Event Registration
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Register for the 2024 Rhode Island Startup Week Event
-        </Typography>
-        <Box sx={{ width: '100%' }}>
-          {/* FormComponent that takes the formConfig as props */}
-          <FormComponent formConfig={eventRegistrationForm()} />
-        </Box>
-      </Box>
-    </Box>
-  );
+	return (
+		// Main container for the page
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'flex-start',
+				justifyContent: 'flex-start',
+				width: '100%',
+				height: '100%',
+				mt: '2rem',
+				position: 'relative',
+			}}
+		>
+			{/* Box component that wraps the form */}
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'flex-start',
+					justifyContent: 'center',
+					width: '100%',
+					maxWidth: '400px',
+					padding: '2rem',
+					border: '1px solid #252b4e',
+					borderRadius: '8px',
+					position: 'relative',
+					left: '-300px',
+					mt: '2rem',
+                    backgroundColor: '#252b4e',
+                    color: '#fff',
+				}}
+			>
+				<Box
+                sx={{
+                    textAlign: 'center',
+                    width: '100%',
+                }}
+                >
+					<Typography variant="h5" gutterBottom>
+						Event Registration
+					</Typography>
+					<Typography variant="body2" gutterBottom>
+						Register for the 2024 Rhode Island Startup Week Event
+					</Typography>
+				</Box>
+				<Box sx={{ width: '100%' }}>
+					{/* FormComponent that takes the formConfig as props */}
+					<FormComponent formConfig={eventRegistrationForm()} />
+				</Box>
+			</Box>
+		</Box>
+	);
 };
 
 export default page;
