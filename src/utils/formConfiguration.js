4ef8c78b45971e90c3ee66c9c432e0063a3de8cf
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-06 21:11:53 -0400

    Created configuration for registration form in utils

---
+++ src/utils/formConfiguration.js
@@ -0,0 +1,41 @@
+// Form Configuration
+export const eventRegistrationForm = {
+    fields: [
+        {
+            name: 'firstName',
+            label: 'First Name',
+            required: true,
+            helperText: 'Please enter your first name',
+        },
+        {
+            name: 'lastName',
+            label: 'Last Name',
+            required: true,
+            helperText: 'Please enter your last name',
+        },
+        {
+            name: 'email',
+            label: 'Email',
+            required: true,
+            helperText: 'Please enter your email',
+        },
+        {
+            name: 'phone',
+            label: 'Phone Number',
+            required: true,
+            helperText: 'Please enter your phone number',
+        },
+        {
+            name: 'event',
+            label: 'Event',
+            required: true,
+            helperText: 'Please enter the event name',
+        },
+        {
+            name: 'affiliation',
+            label: 'Affiliation',
+            required: false,
+            helperText: 'Please enter your affiliation',
+        }
+    ],
+}; 
