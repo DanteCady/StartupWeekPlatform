c4586a427351d3d1c44a1ee7fab470e19d7b8aca
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-08 14:33:38 -0400

    Modularized and refactored backend routes

--- backend/routes/eventsRoutes.js
+++ backend/routes/eventsRoutes.js
@@ -3,38 +3,49 @@ const router = express.Router(); // Express router
 
 // Module exports function that takes a database pool as an argument and returns the router object
 module.exports = (databasePool) => {
-    // ################ Routes ################ //
+	// ################ Routes ################ //
+	// ############################################ //
 
-    // Get all events
-    router.get('/all', async (req, res) => {
-        try {
-            const query = 'SELECT * FROM events'; // Define the query
-            const [events] = await databasePool.query(query); // Execute the query
-            res.status(200).json(events); // Send the events data back to the client
-        } catch (error) {
-            res.status(500).json({ error: 'Error fetching events' }); // Send an error response
-        }
-    });
+	// Get all events
+	router.get('/all', getAllEvents);
+	// Register for an event
+	router.post('/register', registerForEvent);
 
-    // Register for an event
-    router.post('/register', async (req, res) => {
-        const { eventId, registrationId } = req.body; // Get eventId and registrationId from the request body
+	// ################ Route Handlers ################ //
+	// ############################################ //
+	// Get all events
+	async function getAllEvents(req, res) {
+		try {
+			const query = 'SELECT * FROM events'; // Define the query
+			const [events] = await databasePool.query(query); // Execute the query
+			res.status(200).json(events); // Send the events data back to the client
+		} catch (error) {
+			res.status(500).json({ error: 'Error fetching events' }); // Send an error response
+		}
+	}
 
-        if (!eventId || !registrationId) {
-            return res.status(400).json({ error: 'Missing eventId or registrationId' }); // Validate input
-        }
+	// Register for an event
+	async function registerForEvent(req, res) {
+		const { eventId, registrationId } = req.body; // Get eventId and registrationId from the request body
 
-        try {
-            // Insert the registration details into the 'registrations' table
-            const query = 'INSERT INTO events_registrations (eventId, registrationId, createdAt) VALUES (?, ?, NOW())';
-            await databasePool.query(query, [eventId, registrationId]); // Execute the query
+		if (!eventId || !registrationId) {
+			return res
+				.status(400)
+				.json({ error: 'Missing eventId or registrationId' }); // Validate input
+		}
 
-            res.status(201).json({ message: 'Registration successful' }); // Send success response
-        } catch (error) {
-            console.error('Error registering for the event:', error); // Log the error
-            res.status(500).json({ error: 'Error registering for the event' }); // Send error response
-        }
-    });
+		try {
+			// Insert the registration details into the 'registrations' table
+			const query =
+				'INSERT INTO events_registrations (eventId, registrationId, createdAt) VALUES (?, ?, NOW())';
+			await databasePool.query(query, [eventId, registrationId]); // Execute the query
 
-    return router; // Return the router object
+			res.status(201).json({ message: 'Registration successful' }); // Send success response
+		} catch (error) {
+			console.error('Error registering for the event:', error); // Log the error
+			res.status(500).json({ error: 'Error registering for the event' }); // Send error response
+		}
+	}
+
+	return router; // Return the router object
 };
--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -1,32 +1,43 @@
 const express = require('express'); // Express web server framework
 const router = express.Router(); // Express router
 
-
 // Module exports function that takes a database pool as an argument and returns the router object
 module.exports = (databasePool) => {
-
-    // ################ Routes ################ //
+	// ################ Routes ################ //
 	// ############################################ //
 
-    router.post('/submit', async (req, res) => {
-        try {
-            const { firstName, lastName, email, phoneNumber, affiliation } = req.body;
-    
-            // Generate random registration ID
-            const registrationIdNumber = Math.floor(Math.random() * 10000);
-            const registrationID = `RSW-${firstName[0]}${lastName[0]}-${registrationIdNumber}`;
-    
-            const query = 'INSERT INTO registrants (registrationId, firstName, lastName, email, phoneNumber, affiliation, createdAt) VALUES (?, ?, ?, ?, ? ,?, NOW())';
-            const [result] = await databasePool.query(query, [registrationID, firstName, lastName, email, phoneNumber, affiliation]);
-            
-            // Send the registration ID back to the client
-            res.status(200).json({ registrationID });
-        } catch (error) {
-            res.status(500).send(error);
-        }
-    });
-    
+	// Submit form data
+	router.get('/submit', submitFormRegistration);
 
+	// ################ Route Handlers ################ //
+	// ############################################ //
 
-    return router;
-}
+	// Handle form submission
+	async function submitFormRegistration(req, res) {
+		try {
+			const { firstName, lastName, email, phoneNumber, affiliation } = req.body;
+
+			// Generate random registration ID
+			const registrationIdNumber = Math.floor(Math.random() * 10000);
+			const registrationID = `RSW-${firstName[0]}${lastName[0]}-${registrationIdNumber}`;
+
+			const query =
+				'INSERT INTO registrants (registrationId, firstName, lastName, email, phoneNumber, affiliation, createdAt) VALUES (?, ?, ?, ?, ? ,?, NOW())';
+			const [result] = await databasePool.query(query, [
+				registrationID,
+				firstName,
+				lastName,
+				email,
+				phoneNumber,
+				affiliation,
+			]);
+
+			// Send the registration ID back to the client
+			res.status(200).json({ registrationID });
+		} catch (error) {
+			res.status(500).send(error);
+		}
+	}
+
+	return router;
+};
