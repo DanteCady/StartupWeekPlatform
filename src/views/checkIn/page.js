e416a4bdad8b2e692d056afef787df3a712c999c
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-09 17:39:20 -0400

    updated check-in screen ui

--- src/views/checkIn/page.js
+++ src/views/checkIn/page.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import useCheckIn from '../../hooks/checkIn'; 
+import Logo from '../../assets/2023-RI-startup-week-logo.png';   
+
 
 const CheckInPage = () => {
     const { eventId } = useParams(); // Extract eventId from the URL path
@@ -18,17 +20,19 @@ const CheckInPage = () => {
     };
 
     // Split eventId eventiD to get the actual ID
-    const id = eventId.split('=')[1];
+    const id = eventId.split('=')[1]; // Extract the actual ID from the URL by splitting the string at '=' and getting the second part
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 4 }}>
+            <img src={Logo} alt="RI Startup Week Logo" style={{ width: '250px' }} />
+            <hr style={{border: '1px solid black', width: '100%'}}/>
             <Typography variant="h4">Event Check-In</Typography>
             <Typography variant="body1" sx={{ marginBottom: 2 }}>
                 Event ID: {id}
             </Typography>
 
             <TextField
-                label="Enter Registrant ID"
+                label="Enter Your Registrant ID"
                 variant="outlined"
                 value={registrantId}
                 onChange={(e) => setRegistrantId(e.target.value)}
