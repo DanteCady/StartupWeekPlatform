2283496876759b989c26ee0af913fd09885d6c47
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-10 11:17:45 -0400

    Added sign in button

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -1,24 +1,40 @@
-import React from 'react'
-import { Box } from '@mui/material';
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
 
 const Header = () => {
-  return (
-    <Box
-    sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        backgroundColor: '#252b4e',
-        height: '80px',
-        position: 'fixed',
-        zIndex: 1000
-    }}
-    >
-    </Box>
-  )
-}
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'row',
+				alignItems: 'center',
+				justifyContent: 'center',
+				width: '100%',
+				backgroundColor: '#252b4e',
+				height: '80px',
+				position: 'fixed',
+				zIndex: 1000,
+			}}
+		>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'flex-end',
+					width: '100%',
+					maxWidth: '1200px',
+					padding: '0 20px',
+				}}
+			>
+				<Button>
+					<Typography variant="h6" sx={{ color: 'white' }}>
+						Sign In
+					</Typography>
+				</Button>
+			</Box>
+		</Box>
+	);
+};
 
-
-export default Header
+export default Header;
