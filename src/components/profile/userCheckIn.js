464ed371c97001dce092903d95c4538225aa2780
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-14 19:21:22 -0400

    Updated check-ins with pagination

--- src/components/profile/userCheckIn.js
+++ src/components/profile/userCheckIn.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Box, Typography, CircularProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, useMediaQuery, useTheme } from '@mui/material';
-import useRegistrantCheckIns from '../../hooks/getRegistrantCheckIns'; 
+import React, { useState } from 'react';
+import {
+  Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
+  Paper, TablePagination, CircularProgress, Typography, useMediaQuery, useTheme
+} from '@mui/material';import useRegistrantCheckIns from '../../hooks/getRegistrantCheckIns'; 
 import moment from 'moment'; 
 
 const CheckInsTable = ({ registrantId }) => {
   const { checkIns, loading, error } = useRegistrantCheckIns(registrantId);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
   
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0); // Reset to the first page when rows per page changes
+  };
 
   if (loading) {
     return (
@@ -34,6 +48,7 @@ const CheckInsTable = ({ registrantId }) => {
   }
 
   return (
+    <Box sx={{ width: '100%' }}>
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: isMobile ? 300 : 650 }} aria-label="check-ins table">
         <TableHead>
@@ -58,6 +73,15 @@ const CheckInsTable = ({ registrantId }) => {
         </TableBody>
       </Table>
     </TableContainer>
+      <TablePagination
+      component="div"
+      count={checkIns.length}
+      page={page}
+      onPageChange={handleChangePage}
+      rowsPerPage={rowsPerPage}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+    />
+    </Box>
   );
 };
 
