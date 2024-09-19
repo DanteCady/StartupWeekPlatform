6514e193bd277f4ec33658fa0a7227181d8c506b
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-09-19 15:41:34 -0400

    Update to sign In process

--- src/hooks/checkIn.js
+++ src/hooks/checkIn.js
@@ -1,32 +1,42 @@
 import { useState } from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 
 const useCheckIn = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const eventsEndpoint = process.env.REACT_APP_EVENTS_ENDPOINT;
 
-    const checkIn = async (eventId, registrantId) => {
+    const checkIn = async (eventIds, registrantId) => {
         setLoading(true);
         setError(null);
 
         try {
-            const response = await axios.post(`${eventsEndpoint}/check-in`, {
-                eventId,
-                registrantId
-            });
+            // Check if eventIds is an array or single eventId
+            const eventIdArray = Array.isArray(eventIds) ? eventIds : [eventIds];
 
-            // If check-in was successful, return the message
-            return response.data;
+            // Iterate over all eventIds and perform the check-in
+            const checkInResults = await Promise.all(
+                eventIdArray.map(async (eventId) => {
+                    try {
+                        const response = await axios.post(`${eventsEndpoint}/check-in`, {
+                            eventId,
+                            registrantId
+                        });
+                        return response.data;
+                    } catch (err) {
+                        const errorMessage = err.response?.data?.error;
+                        if (errorMessage === 'User has already checked in for this event') {
+                            return { message: errorMessage }; // Treat this as a valid response
+                        }
+                        throw new Error(errorMessage || 'An unknown error occurred');
+                    }
+                })
+            );
+
+            // Return the results of all check-ins
+            return checkInResults;
         } catch (err) {
-            const errorMessage = err.response?.data?.error;
-            // If the error message is "User has already checked in for this event", return that as a response
-            if (errorMessage === 'User has already checked in for this event') {
-                return { message: errorMessage }; // Treat this as a valid response
-            }
-
-            // Handle other errors as actual errors
-            setError(errorMessage || 'An unknown error occurred');
+            setError(err.message);
             return null;
         } finally {
             setLoading(false);
